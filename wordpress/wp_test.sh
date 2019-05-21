603dcf29eb86ac8267468d031358faf1ecb7c9b2
Lucas Ducau <lucas.ducau@globant.com>
2019-05-21 12:06:40 -0300

    added test

--- wordpress/wp_test.sh
+++ wordpress/wp_test.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 
-curl http://localhost:80/
+curl http://localhost:80/ && echo "Tests passed." || ( echo "Tests failed." && exit 1 )
 
-if [[ "${?}" -eq 0]]
-then
-	echo "Tests passed."
-else
-	echo "Tests failed."
-	exit 1
-fi
-
+#if [[ "${?}" -eq 0]]
+#then
+#	echo "Tests passed."
+#else
+#	echo "Tests failed."
+#	exit 1
+#fi
